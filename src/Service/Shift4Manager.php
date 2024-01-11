8840066c5751c72de98b3a2cb27d063d0fc1c616
s-Lorenc.Doda <s-lorenc.doda@subsplash.com>
2024-01-11 16:40:30 +0100

    Commit main 2

--- src/Service/Shift4Manager.php
+++ src/Service/Shift4Manager.php
@@ -52,6 +52,9 @@ class Shift4Manager
         return $customer;
     }
 
+    /**
+     * @return array|array[]
+     */
     public function getPaymentsList()
     {
         /** @var ListResponse $response */
